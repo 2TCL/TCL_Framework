97ebb46fe696939844c38def4aabb1c86e86892e
Lundi <49914842+minhluannguyen@users.noreply.github.com>
2022-01-10 18:26:53 +0700

    Mapper

---
+++ TCL_Framework/Abstractions/TCLMapper.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCL_Framework.Abstractions;
+using TCL_Framework.Attributes;
+
+namespace TCL_Framework.Interfaces
+{
+    public abstract class TCLMapper
+    {
+        protected abstract void OneToManyMapping<T>(TCLConnection cnn, DataRow dr, T obj) where T : new();
+        protected abstract void ToOneMapping<T>(TCLConnection cnn, DataRow dr, T obj) where T : new();
+
+        public T RelationshipMapping<T>(TCLConnection cnn, DataRow dr) where T : new()
+        {
+            T obj = new T();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+                var columnMapping = FirstOrDefault(attributes, typeof(ColumnAttribute));
+
+                if (columnMapping != null)
+                {
+                    var mapsTo = columnMapping as ColumnAttribute;
+                    property.SetValue(obj, dr[mapsTo.Name]);
+                }
+            }
+
+            OneToManyMapping(cnn, dr, obj);
+            ToOneMapping(cnn, dr, obj);
+
+            return obj;
+        }
+
+        public T NoRelationshipMapping<T>(TCLConnection cnn, DataRow dr) where T : new()
+        {
+            T obj = new T();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+
+                var columnMapping = FirstOrDefault(attributes, typeof(ColumnAttribute));
+                if (columnMapping != null)
+                {
+                    var mapsTo = columnMapping as ColumnAttribute;
+                    property.SetValue(obj, dr[mapsTo.Name]);
+                }
+            }
+
+            return obj;
+        }
+
+        public string GetTableName<T>() where T : new()
+        {
+            var tableAttributes = typeof(T).GetCustomAttributes(typeof(TableAttribute), true);
+            var tableAttribute = FirstOrDefault(tableAttributes, typeof(TableAttribute)) as TableAttribute;
+            if (tableAttribute != null)
+                return tableAttribute.Name;
+            return string.Empty;
+        }
+
+        public List<PrimaryKeyAttribute> GetPrimaryKeys<T>() where T : new()
+        {
+            List<PrimaryKeyAttribute> primaryKeys = new List<PrimaryKeyAttribute>();
+
+            var properties = typeof(T).GetProperties();
+
+            foreach(var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+                var primaryKeyAttribute = FirstOrDefault(attributes, typeof(PrimaryKeyAttribute));
+                if (primaryKeyAttribute != null)
+                    primaryKeys.Add(primaryKeyAttribute as PrimaryKeyAttribute);
+            }
+
+            if (primaryKeys.Count > 0)
+                return primaryKeys;
+            else
+                return null;
+        }
+
+        public List<ForeignKeyAttribute> GetForeignKeys<T>(string relationshipID) where T : new()
+        {
+            List<ForeignKeyAttribute> foreignKeys = new List<ForeignKeyAttribute>();
+
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+                var foreignKeyAttribute = FirstOrDefault(attributes, typeof(ForeignKeyAttribute));
+                if (foreignKeyAttribute != null && (foreignKeyAttribute as ForeignKeyAttribute).RelationshipId == relationshipID)
+                    foreignKeys.Add(foreignKeyAttribute as ForeignKeyAttribute);
+            }
+
+            if (foreignKeys.Count > 0)
+                return foreignKeys;
+            else
+                return null;
+        }
+
+        public List<ColumnAttribute> GetColumns<T>() where T : new()
+        {
+            List<ColumnAttribute> columnAttributes = new List<ColumnAttribute>();
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+                var columnMapping = FirstOrDefault(attributes, typeof(ColumnAttribute));
+
+                if (columnMapping != null)
+                {
+                    var mapsTo = columnMapping as ColumnAttribute;
+                    columnAttributes.Add(mapsTo);
+                }
+            }
+
+            if (columnAttributes.Count > 0)
+                return columnAttributes;
+            else
+                return null;
+        }
+
+        public Dictionary<ColumnAttribute, object> GetColumnsWithValues<T>(T obj)
+        {
+            Dictionary<ColumnAttribute, object> listColumnValues = new Dictionary<ColumnAttribute, object>();
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+                var columnMapping = FirstOrDefault(attributes, typeof(ColumnAttribute));
+
+                if (columnMapping != null)
+                {
+                    var mapsTo = columnMapping as ColumnAttribute;
+                    listColumnValues.Add(mapsTo, property.GetValue(obj, null));
+                }
+            }
+
+            if (listColumnValues.Count > 0)
+                return listColumnValues;
+            else
+                return null;
+        }
+
+        public ColumnAttribute GetColumn(string name, Dictionary<ColumnAttribute, object> listColumValues)
+        {
+            foreach (ColumnAttribute column in listColumValues.Keys)
+                if (column.Name == name)
+                    return column;
+            return null;
+        }
+
+        public ColumnAttribute GetColumn(string name, List<ColumnAttribute> listColumAttributes)
+        {
+            foreach (ColumnAttribute column in listColumAttributes)
+                if (column.Name == name)
+                    return column;
+            return null;
+        }
+
+        protected object FirstOrDefault(object[] attributes, Type type)
+        {
+            foreach (var a in attributes)
+            {
+                if (a.GetType() == type)
+                    return a;
+            }
+            return null;
+        }
+
+        protected object[] GetAll(object[] attributes, Type type)
+        {
+            object[] objArray = new object[0];
+            foreach (var a in attributes)
+            {
+                if (a.GetType() == type)
+                {
+                    Array.Resize(ref objArray, objArray.Length + 1);
+                    objArray[objArray.Length - 1] = a;
+                }
+            }
+            return objArray;
+        }
+
+        public object GetFirst(IEnumerable source)
+        {
+            IEnumerator iter = source.GetEnumerator();
+
+            if (iter.MoveNext())
+            {
+                return iter.Current;
+            }
+            return null;
+        }
+    }
+}
---
+++ TCL_Framework/TCLPostgreSQL/TCLPostgreSQLMapper.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TCL_Framework.Abstractions;
+using TCL_Framework.Attributes;
+using TCL_Framework.Enums;
+using TCL_Framework.Interfaces;
+
+namespace TCL_Framework.TCLPostgreSQL
+{
+    public class TCLPostgreSQLMapper : TCLMapper
+    {
+        protected override void OneToManyMapping<T>(TCLConnection cnn, DataRow dr, T obj)
+        {
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+                var oneToManyAttributes = GetAll(attributes, typeof(OneToManyAttribute));
+
+                if (oneToManyAttributes != null && oneToManyAttributes.Length != 0)
+                {
+                    foreach (OneToManyAttribute oneToManyAttribute in oneToManyAttributes)
+                    {
+                        Type type = property.PropertyType;
+                        if (type.IsGenericType)
+                        {
+                            Type itemType = type.GetGenericArguments()[0];
+                            TCLPostgreSQLMapper mapper = new TCLPostgreSQLMapper();
+
+                            MethodInfo getTableNameMethod = mapper.GetType().GetMethod("GetTableName")
+                               .MakeGenericMethod(new Type[] { itemType });
+                            string tableName = getTableNameMethod.Invoke(mapper, null) as string;
+
+                            MethodInfo getForeignKeyAttributeMethod = mapper.GetType().GetMethod("GetForeignKeys")
+                                .MakeGenericMethod(new Type[] { itemType });
+                            List<ForeignKeyAttribute> foreignKeyAttributes = getForeignKeyAttributeMethod.Invoke(mapper, new object[] { oneToManyAttribute.RelationshipId }) as List<ForeignKeyAttribute>;
+
+                            MethodInfo getColumnAttributeMethod = mapper.GetType().GetMethod("GetColumns")
+                                .MakeGenericMethod(typeof(T));
+                            List<ColumnAttribute> columnAttributes = getColumnAttributeMethod.Invoke(mapper, null) as List<ColumnAttribute>;
+
+                            string whereStr = string.Empty;
+                            if (foreignKeyAttributes != null)
+                            {
+                                foreach (ForeignKeyAttribute foreignKeyAttribute in foreignKeyAttributes)
+                                {
+                                    ColumnAttribute column = GetColumn(foreignKeyAttribute.References, columnAttributes);
+                                    if (column != null)
+                                    {
+                                        string format = "{0} = {1}, ";
+                                        if (column.Type == DataType.NCHAR || column.Type == DataType.NVARCHAR)
+                                            format = "{0} = N'{1}', ";
+                                        else if (column.Type == DataType.CHAR || column.Type == DataType.VARCHAR)
+                                            format = "{0} = '{1}', ";
+
+                                        whereStr += string.Format(format, foreignKeyAttribute.Name, dr[foreignKeyAttribute.References]);
+                                    }
+                                }
+                            }
+                            if (!string.IsNullOrEmpty(whereStr))
+                            {
+                                whereStr = whereStr.Substring(0, whereStr.Length - 2);
+                                string query = string.Format("SELECT * FROM {0} WHERE {1}", tableName, whereStr);
+
+                                cnn.Open();
+                                MethodInfo method = cnn.GetType().GetMethod("ExecuteQueryWithOutRelationship")
+                                .MakeGenericMethod(new Type[] { itemType });
+                                property.SetValue(obj, method.Invoke(cnn, new object[] { query }));
+                                cnn.Close();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        protected override void ToOneMapping<T>(TCLConnection cnn, DataRow dr, T obj)
+        {
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                Type type = property.PropertyType;
+                var attributes = property.GetCustomAttributes(false);
+
+                var arr1 = GetAll(attributes, typeof(OneToOneAttribute));
+                var arr2 = GetAll(attributes, typeof(ManyToOneAttribute));
+
+                var toOneAttributes = new object[arr1.Length + arr2.Length];
+                if (toOneAttributes.Length > 0)
+                {
+                    arr1.CopyTo(toOneAttributes, 0);
+                    arr2.CopyTo(toOneAttributes, arr1.Length);
+                }
+
+                if (toOneAttributes != null && toOneAttributes.Length != 0)
+                {
+                    foreach (var attribute in toOneAttributes)
+                    {
+                        TCLPostgreSQLMapper mapper = new TCLPostgreSQLMapper();
+                        string tableName = string.Empty;
+                        string whereStr = string.Empty;
+                        string relationshipID = string.Empty;
+
+                        if (attribute.GetType() == typeof(OneToOneAttribute))
+                        {
+                            relationshipID = (attribute as OneToOneAttribute).RelationshipId;
+                            tableName = (attribute as OneToOneAttribute).TableName;
+                        }
+                        else
+                        {
+                            relationshipID = (attribute as ManyToOneAttribute).RelationshipId;
+                            tableName = (attribute as ManyToOneAttribute).TableName;
+                        }
+
+                        MethodInfo getForeignKeyAttributeMethod = mapper.GetType().GetMethod("GetForeignKeys")
+                            .MakeGenericMethod(typeof(T));
+                        List<ForeignKeyAttribute> foreignKeyAttributes = getForeignKeyAttributeMethod.Invoke(mapper, new object[] { relationshipID }) as List<ForeignKeyAttribute>;
+
+                        MethodInfo getColumnAttributeMethod = mapper.GetType().GetMethod("GetColumns")
+                            .MakeGenericMethod(new Type[] { type });
+
+                        List<ColumnAttribute> columnAttributes = getColumnAttributeMethod.Invoke(mapper, null) as List<ColumnAttribute>;
+
+                        if (foreignKeyAttributes != null)
+                        {
+                            foreach (ForeignKeyAttribute foreignKeyAttribute in foreignKeyAttributes)
+                            {
+                                ColumnAttribute column = GetColumn(foreignKeyAttribute.References, columnAttributes);
+                                if (column != null)
+                                {
+                                    string format = "{0} = {1}, ";
+                                    if (column.Type == DataType.NCHAR || column.Type == DataType.NVARCHAR)
+                                        format = "{0} = N'{1}', ";
+                                    else if (column.Type == DataType.CHAR || column.Type == DataType.VARCHAR)
+                                        format = "{0} = '{1}', ";
+
+                                    whereStr += string.Format(format, foreignKeyAttribute.References, dr[foreignKeyAttribute.Name]);
+                                }
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(whereStr))
+                        {
+                            whereStr = whereStr.Substring(0, whereStr.Length - 2);
+                            string query = string.Format("SELECT * FROM {0} WHERE {1}", tableName, whereStr);
+
+                            cnn.Open();
+                            MethodInfo method = cnn.GetType().GetMethod("ExecuteQueryWithOutRelationship")
+                            .MakeGenericMethod(new Type[] { type });
+                            var ienumerable = (IEnumerable)method.Invoke(cnn, new object[] { query });
+                            cnn.Close();
+
+                            MethodInfo method2 = mapper.GetType().GetMethod("GetFirst");
+                            var firstElement = method2.Invoke(mapper, new object[] { ienumerable });
+
+                            property.SetValue(obj, firstElement);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
